ed350252d07f3c1459970e5b09888b6be3a83657
mydongistiny <jaysonedson@gmail.com>
2015-01-07 15:43:21 -0800

    Recents: Add recents menu options and floating button[1/2].

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3138,6 +3138,18 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
          /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+         /**
          * Recents Show/Hide Search Bar
          * @hide
          */
